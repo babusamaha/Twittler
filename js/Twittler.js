8c159bd9784d9f82a7d086585cf1d7b5daa98be7
Adam Samaha <adamsamahaa@gmail.com>
2016-02-18 12:50:40 -0800

    generating tweets

--- js/Twittler.js
+++ js/Twittler.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
     var userName = 'Adam Samaha';
     var userImg = 'img/adam.jpg';
-    var userImgLink = '<a href="https://www.github.com/Asamaha" target="_blank"><img alt="" class="img" src="' + userImg + '"><a/>';
+    var userImgLink = '<a href="https://github.com/samaha999" target="_blank"><img alt="" class="img" src="' + userImg + '"><a/>';
     var nameInput = document.getElementById('name');
     var postGrap = document.getElementById('newMessage');
     var nameDisplay = $('#newName').html(userName);
@@ -84,5 +84,40 @@ $(document).ready(function() {
     var wordThree = ["my", "an entire", "this", "the", "the big", "a new form of", "all", "my", "your", "the", "a"];
     var wordFour = ["media", "paparazzi", "country", "people", "dream", "show", "oscar", "world", "government", "city", "system", "cloud", "money", "way of life", "belief system", "bad decisions", "future", "life", "party", "minds", "music", "media", "history", "community", "year", "society", "televison"];
 
+    /* function to display random tweet post from mix arrays of users and words*/
+    var getRandomFromArray = function(arr) {
+        var randomIndex = Math.floor(Math.random() * arr.length);
+        return arr[randomIndex];
+    };
+    /* function to display random users*/
+    var getRandomUsers = function() {
+        return getRandomFromArray(users);
+    };
+    /* function to display random tweet*/
+    var randomMessage = function() {
+        return [getRandomFromArray(wordOne), getRandomFromArray(wordTwo), getRandomFromArray(wordThree), getRandomFromArray(wordFour)].join(' ');
+    };
+
+    /* function to display new random tweets*/
+    function newTweet(msg, usr) {
+        var rendomUser = usr || getRandomUsers();
+        var tweet = '<li ' +
+            'data-id="' + rendomUser.id + '" ' +
+            'data-name="' + rendomUser.name + '" ' +
+            '><a href="#" target="#"><img alt="" class="img" src="' +
+            rendomUser.img + '" /> @ <span class="username">' +
+            rendomUser.name + '</span></a>' + ': ' +
+            '<p class="tweetColor">' + msg + '</p><p class="date">' +
+            new Date() + ' </p></li>';
+        posts.prepend(tweet);
+        // on every new tweet we need to trigget a filter check to filter the new post
+    }
+    /* function to set random time to display new random tweets*/
+    // setInterval(newTweet, (Math.random() * 3000) + 5000);
+    setInterval(function() {
+        newTweet(randomMessage());
+    }, 3000);
+
+    /* function to display new userName , new userImg and alert Hello message */
 
 });
